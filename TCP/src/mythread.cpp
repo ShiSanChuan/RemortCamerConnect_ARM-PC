1922d0eae930716d6ba5f0071b8f35852da815e6
shisanchuan <2399527300@qq.com>
2018-08-14 21:22:35 +0800

    thread??

--- TCP/src/mythread.cpp
+++ TCP/src/mythread.cpp
@@ -2,42 +2,48 @@
 #include <unistd.h>
 #include <thread>
 #include <vector>
-
+#include <map>
 
 class mythread
 {
 private:
 	std::vector<std::thread> threads;
+	std::map<std::string,int> Map;
 public:
 	mythread();
-	~mythread(){}
-	int Add(void (*fun)(void));
-	int Delete(); 
+	~mythread();
+	int Add(const std::string name,void (*fun)(void));
+	int Delete(const std::string name); 
 };
 
 mythread::mythread(){
 	
 }
+mythread::~mythread(){
+
+}
 void back(int i){
 
 }
-int mythread::Add(void (*fun)(void)){
+int mythread::Add(const std::string name,void (*fun)(void)){
 	threads.push_back(std::thread(fun));
-	std::cout<<">>>"<<threads.size()<<std::endl;
+	// std::cout<<">>>"<<threads.size()<<std::endl;
+	Map.insert(std::pair<std::string,int>(name,threads.size()-1));
 	threads[threads.size()-1].detach();
 	return 0;
 }
-int mythread::Delete(){
+int mythread::Delete(const std::string name){
+	// TerminateThread(threads[1]);
 	return 0;
 }
 
-int main(int argc, char const *argv[])
-{
-	mythread m;
-	m.Add([]{std::cout<<"ffff"<<std::endl;});
-	m.Add([]{std::cout<<"ffff"<<std::endl;});
-	m.Add([]{std::cout<<"ffff"<<std::endl;});
-	m.Add([]{std::cout<<"ffff"<<std::endl;});
-	sleep(10);
-	return 0;
-}
+// int main(int argc, char const *argv[])
+// {
+// 	mythread m;
+// 	m.Add("1",[]{std::cout<<"ffff"<<std::endl;});
+// 	m.Add("2",[]{std::cout<<"ffff"<<std::endl;});
+// 	m.Add("3",[]{std::cout<<"ffff"<<std::endl;});
+// 	m.Add("4",[]{std::cout<<"ffff"<<std::endl;});
+// 	sleep(3);
+// 	return 0;
+// }
